5a6e8c56d8f254890709d683fdcd630c35984448
kujovic <stefan.kujovic@saga.rs>
2022-01-28 13:52:31 +0100

    Proba 3 - grana nova

--- src/main/java/rs/saga/mavendemogit/Osoba.java
+++ src/main/java/rs/saga/mavendemogit/Osoba.java
@@ -9,5 +9,36 @@ package rs.saga.mavendemogit;
  * @author stefan.kujovic
  */
 public class Osoba {
+    private int osobaId;
+    private String ime;
+
+    /**
+     * @return the osobaId
+     */
+    public int getOsobaId() {
+        return osobaId;
+    }
+
+    /**
+     * @param osobaId the osobaId to set
+     */
+    public void setOsobaId(int osobaId) {
+        this.osobaId = osobaId;
+    }
+
+    /**
+     * @return the ime
+     */
+    public String getIme() {
+        return ime;
+    }
+
+    /**
+     * @param ime the ime to set
+     */
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+    
     
 }
