2e52ae55ed8daa3da7407f96c734b68402ec9d89
kujovic <stefan.kujovic@saga.rs>
2022-01-28 13:33:29 +0100

    Maven projekat

---
+++ src/main/java/rs/saga/mavendemogit/Osoba.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rs.saga.mavendemogit;
+
+/**
+ *
+ * @author stefan.kujovic
+ */
+public class Osoba {
+    
+}
